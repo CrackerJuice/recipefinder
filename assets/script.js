40f8205e508c6a70640559ba0470be439519d794
mercurioregon <jamesm620@gmail.com>
2024-03-26 15:55:58 -0700

    css and html updates

--- assets/script.js
+++ assets/script.js
@@ -6,33 +6,7 @@ openModalButton.addEventListener("click", function() {
     modal.classList.remove("hidden");
 });
 
-// closeModalSpan.addEventListener("click", function() {
-//     modal.classList.add("hidden");
-// });
-
-//  ****AARON SPOON CODE
-
-// function getsource(id){
-//     $.ajax({
-//     url:"https://api.spoonacular.com/recipes/"+id+"/information?apiKey=91ca1bbf73cc47f8bb9b7df0e16f5855",
-//     success:function(res){
-//         document.getElementById("sourceLink")InnerHTML=res.sourceUrl
-//         document.getElementById("sourceLink").href=res.sourceUrl
-//     }
-//     });
-//     }
-//     function getrecipe(q){
-//         $.ajax({
-//             url:`https://api.spoonacular.com/recipes/search?apiKey=91ca1bbf73cc47f8bb9b7df0e16f5855&number=3&query="+q{ingredient1}+${ingredient2}+${ingredient3}',
-//         success: function(res){
-//             document.getElementById("output").innerHTML="<h1>"+res.results[0].title+</h1><br><img src='""+res.baseUri+res.results[0].image"'+width='400'
-//         }})
-//     }
-
-
 // ****JIM YOUTUBE CODE
-
-
 
 // const searchTerms = ["fish","bacon", "broccoli"];
 
@@ -54,27 +28,11 @@ const searchHandler = (e)=>{
 
 
      
-
-  
-
-
     const url = `https://api.spoonacular.com/recipes/search?apiKey=444f40431ebe48f792441b4ccde7ecca&number=3&includeIngredients="${ingredient1},${ingredient2},${ingredient3}` ;
   
-  
-
-
-
-
-
-
-
-
-
-
-    const youtubeurl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${ingredient1}+${ingredient2}+${ingredient3}&key=AIzaSyCTDKpNNSBmISaHBE4GB_8ibaWi7_bt7-Q`;
+     const youtubeurl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${ingredient1}+${ingredient2}+${ingredient3}&key=AIzaSyCTDKpNNSBmISaHBE4GB_8ibaWi7_bt7-Q`;
 
         
-
 fetch(url)
   .then(response => response.json())
   .then(data => {
@@ -122,20 +80,12 @@ fetch(url)
 
         const nameHtml = document.createElement("h3")
         const nameURL = document.createElement("h5")
-        // const nameImage = document.createElement("img")
-
-        
-        //Put the info into html
-        // nameImage.setAttribute("src", dishImage)
+    
         nameHtml.textContent = dishName
         nameURL.textContent = dishURL
-        // nameImage.textContent = dishImage
-
-        //append the newly created html element
 
         resultsh2.append(nameHtml, nameURL, )
     }
-
   
     console.log(data);
 
@@ -144,9 +94,6 @@ fetch(url)
 }
 const searchButton5= document.getElementById("submit")
 searchButton5.addEventListener("click",searchHandler)
-
-
-
 
 // SET AND GET FROM LOCAL STORAGE 
 
